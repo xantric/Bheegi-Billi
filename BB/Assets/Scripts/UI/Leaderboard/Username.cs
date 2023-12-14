6fa305b22ee251fd6ac3360db6dd6112fca51c36
Sarthak Agarwal <sarthaka21@iitk.ac.in>
2023-12-14 14:44:58 +0530

    UI

--- BB/Assets/Scripts/UI/Leaderboard/Username.cs
+++ BB/Assets/Scripts/UI/Leaderboard/Username.cs
@@ -19,16 +19,7 @@ public class Username: MonoBehaviour
         if (!string.IsNullOrEmpty(user_input.text))
         {
             PlayerPrefs.SetString("username", user_input.text);
-            int leveller = MainMenuController.level;
-
-            if (leveller == 1)
-                SceneManager.LoadScene(7);
-
-            if (leveller == 2)
-                SceneManager.LoadScene(8);
-
-            if (leveller == 3)
-                SceneManager.LoadScene(9);
+            SceneManager.LoadScene(2);
         }
     }
 }
