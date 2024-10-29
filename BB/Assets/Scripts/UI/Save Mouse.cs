80dfd0380736b1c5f7cdd661d4c5d2f3c1027964
Kunal Shikhar <kunalshikhar2@gmail.com>
2024-10-29 14:25:40 +0530

    gamepad mouse fixed

--- BB/Assets/Scripts/UI/Save Mouse.cs
+++ BB/Assets/Scripts/UI/Save Mouse.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem.UI;
 
 public class SaveMouse : MonoBehaviour
 {
     public static SaveMouse instance;
+    VirtualMouseInput gamepad;
     private void Awake()
     {
+        gamepad = GetComponent<VirtualMouseInput>();
         if (instance != null)
         {
             Destroy(gameObject);
@@ -17,5 +21,18 @@ public class SaveMouse : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
 
+    }
+    private void Update()
+    {
+        var connectedGamepads = Input.GetJoystickNames().Where(name => !string.IsNullOrEmpty(name)).ToArray();
+        if (connectedGamepads.Length == 0)
+        {
+            gamepad.enabled = false;
+        }
+        else
+        {
+            gamepad.enabled = true;
+        }
+        //Debug.Log(connectedGamepads.Length);
     }
 }
